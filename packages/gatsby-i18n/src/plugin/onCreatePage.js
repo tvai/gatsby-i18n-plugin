1187bb33b9cc7a3f2f6d0ee3cf2aa2c6c38472a1
daniel fein <danielfein@NYC-US07072-MBP.local>
2019-01-18 12:23:17 -0500

    Change to allow fallback langauge to be hidden from the URL (e.g. /en-US/page becomes /page), and to disable redirect if not necessary for your project.

--- packages/gatsby-i18n/src/plugin/onCreatePage.js
+++ packages/gatsby-i18n/src/plugin/onCreatePage.js
@@ -2,36 +2,38 @@ import path from 'path';
 
 const onCreatePage = ({ page, actions }, pluginOptions) => {
   const { createPage, deletePage } = actions;
-  const { fallbackLng, availableLngs, siteUrl, debug } = pluginOptions;
+  const { fallbackLng, availableLngs, siteUrl, debug, redirectPages, hideFallbackLng } = pluginOptions;
 
   if (page.path.includes('dev-404')) {
     return Promise.resolve();
   }
 
   return new Promise(resolve => {
-    const redirect = path.resolve('./.cache/@igorko/redirect.js');
-    const redirectPage = {
-      ...page,
-      component: redirect,
-      context: {
-        ...page.context,
-        availableLngs,
-        fallbackLng,
-        debug,
-        lng: null,
-        routed: false,
-        redirectPage: page.path,
-        siteUrl,
-      },
-    };
+    if(redirectPages){
+      const redirect = path.resolve('./.cache/@igorko/redirect.js');
+      const redirectPage = {
+        ...page,
+        component: redirect,
+        context: {
+          ...page.context,
+          availableLngs,
+          fallbackLng,
+          debug,
+          lng: null,
+          routed: false,
+          redirectPage: page.path,
+          siteUrl,
+        },
+      };
 
-    deletePage(page);
-    createPage(redirectPage);
+      deletePage(page);
+      createPage(redirectPage);
+    }
 
     availableLngs.forEach(lng => {
       const localePage = {
         ...page,
-        path: `/${lng}${page.path}`,
+        path: lng == fallbackLng && hideFallbackLng ? page.path : `/${lng}${page.path}`,
         context: {
           ...page.context,
           availableLngs,
